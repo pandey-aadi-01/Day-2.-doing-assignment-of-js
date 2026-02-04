864aefcf56c05bcc3f410e9f5fa1796b7802bf90
Your Name <your-email@example.com>
2026-02-04 22:07:41 +0530

    Initial commit

--- day 2/srcript.js
+++ day 2/srcript.js
@@ -25,16 +25,33 @@
 
 // console.log(swapped)
 // *Flatten object of arrays into one array**
-const data = {
-  fruits: ["apple", "banana"],
-  veggies: ["carrot", "pea"],
-};
+// const data = {
+//   fruits: ["apple", "banana"],
+//   veggies: ["carrot", "pea"],
+// };
 
-const result = [];
+// const result = [];
 
-for (let key in data) {
-  result.push(...data[key]);
+// for (let key in data) {
+//   result.push(...data[key]);
+// }
+
+// console.log(result);
+//  6.Group people by city
+const input = [
+  { name: "A", city: "Delhi" },
+  { name: "B", city: "Mumbai" },
+  { name: "C", city: "Delhi" },
+];
+
+const result = {};
+
+for (let i = 0; i < input.length; i++) {
+  if (!result[input[i].city]) {
+    result[input[i].city] = [];
+  }
+  if (result[input[i].city]) {
+    result[input[i].city].push(input[i].name);
+  }
 }
-
 console.log(result);
-https://github.com/pandey-aadi-01/Day-2.-doing-assignment-of-js
